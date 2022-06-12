e709ec19995813f8dd31a79a3d7cdbb3ed9f35dd
cynamon jules <julescweb@mail.com>
2022-06-12 18:17:03 +0200

    mise en place

---
+++ site_de_dj/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './Component/Header/Header';
+import Nav from './Component/Nav/Nav';
+import About from './Component/About/About';
+import Gallery from './Component/Gallery/Gallery';
+import Engagement from './Component/Engagement/Engagement';
+import Contact from './Component/Contact/Contact';
+import Footer from './Component/Footer/Footer';
+import Trending from './Component/Trending/Trending';
+
+
+function App() {
+	return (
+		<div className='App'>
+			<>
+				<Header />
+				<Nav />
+				<About />
+				<Gallery />
+				<Engagement />
+				<Trending />
+				<Contact />
+				<Footer />
+			</>
+		</div>
+	);
+}
+
+export default App;
---
+++ site_de_dj/src/Component/Engagement/Engagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Engagement() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
